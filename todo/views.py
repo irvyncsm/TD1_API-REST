e4c90dabc8b6e507182d09ba81f713adfba1882e
Coursimault Irvyn <o22206139@info07-12.iut45.univ-orleans.fr>
2024-01-22 09:32:45 +0100

    ajout update_task

--- todo/views.py
+++ todo/views.py
@@ -13,7 +13,7 @@ def make_public_task(task):
 
 @app.route('/todo/api/v1.0/tasks' , methods = ['GET'])
 def get_tasks():
-    return jsonify({'tasks': [make_public_task(t) for t in tasks]})
+    return jsonify (tasks =[ make_public_task (t) for t in tasks ])
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>' , methods = ['GET'])
 def get_task(task_id):
@@ -43,3 +43,20 @@ def not_found(error):
 def bad_request(error):
     return make_response(jsonify({'error': 'Bad request'}), 400)
 
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task)==0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != str:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not str:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title']=request.json.get('title',task[0]['title'])
+    task[0]['description']=request.json.get('description',task[0]['description'])
+    task[0]['done']=request.json.get('done',task[0]['done'])
+    return jsonify({'task': make_public_task(task[0])})
