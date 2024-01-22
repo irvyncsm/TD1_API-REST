38d8e0f53d63c7a0923cd12368643902edfd4635
Coursimault Irvyn <o22206139@info07-12.iut45.univ-orleans.fr>
2024-01-22 09:19:42 +0100

    ajouts gestions erreurs

--- todo/views.py
+++ todo/views.py
@@ -14,3 +14,32 @@ def make_public_task(task):
 @app.route('/todo/api/v1.0/tasks' , methods = ['GET'])
 def get_tasks():
     return jsonify({'tasks': [make_public_task(t) for t in tasks]})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>' , methods = ['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task)==0:
+        abort(404)
+    return jsonify({'task': make_public_task(task[0])})
+
+@app.route('/todo/api/v1.0/tasks', methods = ['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description' , ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': make_public_task(task)}), 201
+
+@app. errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app. errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
